ec9f90fa95f29d06c700932e74785841a2add7ef
liupeng <liupeng_internet@163.com>
2016-08-02 18:31:46 +0800

    Exercise Initial

---
+++ GenerateAESCode/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace GenerateAESCode
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //1.生成AES加密后的AppKey
+            //原始appkey
+            string appKey = "JH01334b46d0c362da33daa2c039e3035d";
+            //密码
+            string password = "YuanBei";
+            //秘钥
+            string iv = "9L3Ukxh0sfhiZK3g";
+            var newAppkey = Helper.AesEncrypt(appKey, password, iv);
+            Console.WriteLine("原始的Appkey是：" + appKey);
+            Console.WriteLine("Aes加密后的appKey是:" + newAppkey);
+            Console.WriteLine("\n");
+
+            //2.解析原始的appKey
+            var oldAppKey = Helper.AesDecrypt(newAppkey, password, iv);
+            Console.WriteLine("解密后的原始Appkey是：" + oldAppKey);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Infruesture/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Infruesture
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            //1.随机数据
+            //for (int i = 0; i < 100; i++)
+            //{
+            //    var result = GetRandomNum(GetRandomSeed(), 1.50, 1.70, 5);
+            //    Console.WriteLine("当前的随机数是：" + result);
+            //}
+
+            //2.类型转换
+            //var emptyNum = "";
+            //int parseResult = int.Parse(null);
+            //Console.WriteLine(parseResult);
+            int convertIntResult = Convert.ToInt32(null);
+            Console.WriteLine(convertIntResult);
+            //int numResult;
+            //var tryParseResult = int.TryParse(null, out numResult);
+            //Console.WriteLine(tryParseResult);
+            //时间类型的转化
+            //DateTime outDate;
+            //var checkDate = "2016-07-29-999";
+            //var currentDate = DateTime.Now.Date.ToShortDateString();
+            //Console.WriteLine(currentDate);
+            //Console.WriteLine(DateTime.TryParse(checkDate, out outDate));
+            Console.ReadLine();
+        }
+
+
+        /// <summary>
+        /// 获取随机数
+        /// </summary>
+        /// <param name="minNum"></param>
+        /// <param name="maxNum"></param>
+        /// <param name="len"></param>
+        /// <returns></returns>
+        public static double GetRandomNum(double minNum, double maxNum, int len)
+        {
+            byte[] bytes = new byte[4];
+            System.Security.Cryptography.RNGCryptoServiceProvider rngObj = new RNGCryptoServiceProvider();
+            rngObj.GetBytes(bytes);
+            var seed = BitConverter.ToInt32(bytes, 0);
+            var random = new Random(seed);
+            return Math.Round(random.NextDouble()*(maxNum - minNum) + minNum, len);
+        }
+    }
+}
