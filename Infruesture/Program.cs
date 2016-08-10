27567f41653930c57554390f8655070adf92228e
liupeng <liupeng_internet@163.com>
2016-08-10 19:07:03 +0800

    HashTable的使用

--- Infruesture/Program.cs
+++ Infruesture/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -241,6 +241,54 @@ namespace Infruesture
              */
 
             //14.子类调用父类的构造函数
+            //15.HashTable的使用
+            // 创建一个Hashtable实例
+            //Hashtable ht = new Hashtable();
+
+            //// 添加keyvalue键值对
+            //ht.Add("A", "1");
+            //ht.Add("B", "2");
+            //ht.Add("C", "3");
+            //ht.Add("D", "4");
+            //15.1 HashTable中不允许存在相同的Key，此处会抛错
+            //ht.Add("D", "4");
+
+            // 遍历哈希表
+            //foreach (DictionaryEntry de in ht)
+            //{
+            //    Console.WriteLine("Key -- {0}; Value --{1}.", de.Key, de.Value);
+            //}
+
+            //// 哈希表排序
+            //ArrayList akeys = new ArrayList(ht.Keys);
+            //akeys.Sort();
+            //foreach (string skey in akeys)
+            //{
+            //    Console.WriteLine("{0, -15} {1, -15}", skey, ht[skey]);
+
+            //}
+
+            // 判断哈希表是否包含特定键,其返回值为true或false
+            //if (ht.Contains("A"))
+            //    Console.WriteLine(ht["A"]);
+
+            // 给对应的键赋值
+            //ht["A"] = "你好";
+
+            //// 移除一个keyvalue键值对
+            //ht.Remove("C");
+
+            //// 遍历哈希表
+            //foreach (DictionaryEntry de in ht)
+            //{
+            //    Console.WriteLine("Key -- {0}; Value --{1}.", de.Key, de.Value);
+            //}
+
+            //// 移除所有元素
+            //ht.Clear();
+
+            //// 此处将不会有任何输出
+            //Console.WriteLine(ht["A"]);
 
 
             Console.Read();
@@ -254,14 +302,12 @@ namespace Infruesture
         {
             public SeniorEnginerOfSon(int level) : base(level)
             {
-
             }
 
             public SeniorEnginerOfSon(int level, string skill) : base(level, skill)
             {
             }
         }
-
 
 
         /// <summary>
