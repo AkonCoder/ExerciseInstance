a729a75365ca5aadb889ea58840926a125ef952b
liupeng <lipeng_internet@163.com>
2017-05-02 09:44:36 +0800

    Add: Async And await

---
+++ Infruesture/AsyncAndAwait.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace Infruesture
+{
+    /// <summary>
+    /// async、await用法
+    /// </summary>
+    public class AsyncAndAwait
+    {
+
+    }
+}
